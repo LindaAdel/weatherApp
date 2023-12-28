3d246316a74813eda2d87739896d30a368e74d0a
lindaAdel <lindaadel17@gmail.com>
2023-12-28 14:55:11 +0200

    create xcode project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Linda adel on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
